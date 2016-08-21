cf7e314ab6851b813dae5d9cc953cf324791847a
Humberto Borba <humberos@gmail.com>
2016-08-21 02:59:41 -0300

    sony: kitakami: Set BT default name dynamically
    Use ro.boot.hardware property and naming BT dynamically.
    
    Signed-off-by: Humberto Borba <humberos@gmail.com>
    Change-Id: I5dddc4698c958cf12eccc349f96a9a4ac83f9d6c

--- bluetooth/bdroid_buildcfg.h
+++ bluetooth/bdroid_buildcfg.h
@@ -18,6 +18,33 @@
 #ifndef _BDROID_BUILDCFG_H
 #define _BDROID_BUILDCFG_H
 
+#include <cutils/properties.h>
+#include <string.h>
+
+static inline const char* getBTDefaultName()
+{
+    char device[PROPERTY_VALUE_MAX];
+    property_get("ro.boot.hardware", device, "");
+
+    if (!strcmp("ivy", device)) {
+        return "Xperia Z3+";
+    } else if (!strcmp("karin", device)) {
+        return "Xperia Z4 Tablet";
+    } else if (!strcmp("karin_windy", device)) {
+        return "Xperia Z4 Tablet";
+    } else if (!strcmp("sumire", device)) {
+        return "Xperia Z5";
+    } else if (!strcmp("suzuran", device)) {
+        return "Xperia Z5 Compact";
+    } else if (!strcmp("satsuki", device)) {
+        return "Xperia Z5 Premium";
+    }
+
+    return "Xperia";
+}
+
+#define BTM_DEF_LOCAL_NAME getBTDefaultName()
+
 #define BTM_WBS_INCLUDED     TRUE
 
 #endif
