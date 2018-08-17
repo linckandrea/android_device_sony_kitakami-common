96956dd5441e174eddc26adc252111e4c8c31262
Ethan Chen <intervigil@gmail.com>
2018-08-17 00:17:06 -0700

    kitakami: Add WLAN overlay files
    [linckandrea edit]: sync also wpa service declaration with hima
    
    Change-Id: If240db81aa5ef1ec5588c1fa6ecb8e5b707aff36

--- device-common.mk
+++ device-common.mk
@@ -266,4 +266,8 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
    macaddrsetup
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
+
 $(call inherit-product, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
