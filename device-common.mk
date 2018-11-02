ab54a846d3322966912da27611527341118fb56a
Ethan Chen <intervigil@gmail.com>
2018-11-02 00:32:56 -0700

    kitakami: Don't use hardware/broadcom configuration
    * We should enable the p2p group interface
    
    Change-Id: Ib4470b0404f75efc4b528c6b9e5c9ffa2f5ab3c8

--- device-common.mk
+++ device-common.mk
@@ -269,5 +269,3 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
-
-$(call inherit-product, hardware/broadcom/wlan/bcmdhd/config/config-bcm.mk)
