df82d15ef3a95769955d06a314aa3de14a6611a0
Andrea <linckandrea01@gmail.com>
2021-05-14 10:55:25 +0200

    kitkami: ditch LD_PRELOAD
     * shim istead

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -184,7 +184,8 @@ TARGET_LD_SHIM_LIBS := \
      /system/lib64/libsys-utils.so|libsensor.so \
      /system/lib/libcammw.so|libsensor.so \
      /system/vendor/lib/libizat_core.so|/system/vendor/lib/libshim_gps.so \
-     /system/vendor/lib64/libizat_core.so|/system/vendor/lib64/libshim_gps.so
+     /system/vendor/lib64/libizat_core.so|/system/vendor/lib64/libshim_gps.so \
+     /system/bin/secd|/system/lib64/lib-preload64.so
 
 # SELinux
 include device/qcom/sepolicy-legacy/sepolicy.mk
