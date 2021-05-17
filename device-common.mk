e18cbe8ca3cf98628fce43a137c2173efa610d70
Andrea <linckandrea01@gmail.com>
2021-05-17 02:19:34 +0200

    kitakami: remove unused macaddrsetup

--- device-common.mk
+++ device-common.mk
@@ -319,9 +319,6 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
-PRODUCT_PACKAGES += \
-   macaddrsetup
-
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/p2p_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/p2p_supplicant_overlay.conf \
     $(LOCAL_PATH)/configs/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf
