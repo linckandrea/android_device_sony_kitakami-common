bbdd910ede8d858e5847e8462c8de4652dd8a564
Andrea <linckandrea01@gmail.com>
2021-05-18 19:52:29 +0200

    Revert "kitakami: Disable UIM, broken in OREO"
    This reverts commit 24d583c4a566740944b727a7319c5e0f8c828d69.

--- device-common.mk
+++ device-common.mk
@@ -158,6 +158,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/flashled_calc_parameters.cfg:system/etc/flashled_calc_parameters.cfg
 
+# FM
+PRODUCT_PACKAGES += \
+    FMRadio \
+    brcm-uim-sysfs \
+    libfmjni
+
 # Health
 PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl \
