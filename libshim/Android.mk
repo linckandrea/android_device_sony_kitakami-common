3ebfa4bb89e150c15761e33770c25352aa961cf1
Andrea <linckandrea01@gmail.com>
2021-05-20 23:38:32 +0200

    kitakami: remove unused gps shim
     * oneplus 2 gps works without them

--- libshim/Android.mk
+++ libshim/Android.mk
@@ -40,16 +40,6 @@ include $(BUILD_SHARED_LIBRARY)
 
 include $(CLEAR_VARS)
 
-LOCAL_SRC_FILES := gps_shim.cpp
-
-LOCAL_MODULE := libshim_gps
-LOCAL_MODULE_TAGS := optional
-LOCAL_PROPRIETARY_MODULE := true
-
-include $(BUILD_SHARED_LIBRARY)
-
-include $(CLEAR_VARS)
-
 LOCAL_SRC_FILES := libaudioclient_shim.cpp
 LOCAL_MODULE := libaudioclient_shim
 LOCAL_MODULE_CLASS := SHARED_LIBRARIES
