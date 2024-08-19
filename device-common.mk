eb3d142c88573d73bf01b3d090430de036d64944
linckandrea <linckandrea01@gmail.com>
2024-08-19 16:20:16 +0200

    kitakami: remove overlay-lineage

--- device-common.mk
+++ device-common.mk
@@ -24,8 +24,7 @@ PRODUCT_SOONG_NAMESPACES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Radio config
 ifneq ($(BOARD_HAVE_RADIO),false)
